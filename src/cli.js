d09305029498c5905d9b3885b8c6a00512ab2b7e
elektrosila <kekki.kek@gmail.com>
2024-03-22 16:02:53 +0300

    commit2

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ export const greeting =() => {
 
 console.log('Welcome to the Brain Games!');
 
-const name = readlineSync.question('What is your name? ');
+const username = readlineSync.question('What is your name? ');
 
-console.log(`Hello, ${name}!`)
+console.log(`Hello, ${username}!`)
 }
